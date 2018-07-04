833658acb1f1b53f7d7f6a148489fd7f09a688ce
Emanuele Fumagalli <emanuelef@gmail.com>
2018-07-04 08:57:59 +0100

    exported classes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const utils = require('./src/utils/utils');
+const Flight = require('./src/models/flight');
+const Position = require('./src/models/position');
+const TimedPosition = require('./src/models/timedPosition');
 
 exports.printMsg = () => {
   console.log('Test message');
 };
 
 exports.utils = utils;
+exports.Flight = Flight;
+exports.Position = Position;
+exports.TimedPosition = TimedPosition;
