aa081e84d77fd6fa509083e961ad6972687827f5
Emanuele Fumagalli <emanuelef@gmail.com>
2018-07-04 01:35:58 +0100

    Added missing files

---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+const geolib = require('geolib');
+
+const FEET_TO_METRES_FACTOR = 0.3048;
+
+exports.feetToMetres = feet => Math.round(feet * FEET_TO_METRES_FACTOR);
+exports.metresToFeet = metres => Math.round(metres / FEET_TO_METRES_FACTOR);
+exports.euclideanDistance = (a, b) => Math.round(Math.sqrt(a ** 2 + b ** 2));
+exports.distFrom = (a, b) => geolib.getDistance(a, b);
+
+exports.roundDec = (num, prec) => Math.round(num * 10 ** prec) / 10 ** prec;
+
+exports.degToCompass = (num) => {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+};
+
+exports.secsFromMidnight = (epochms) => {
+    const newDate = new Date(epochms);
+
+    const hours = newDate.getUTCHours() * (60 * 60);
+    const minutes = newDate.getUTCMinutes() * 60;
+    const seconds = newDate.getUTCSeconds();
+    const secsSinceMidnight = hours + minutes + seconds;
+
+    return secsSinceMidnight;
+};
