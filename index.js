b952ad1a0d4e7fabb4dcb2f2f00e34aea7493599
Emanuele Fumagalli <emanuelef@gmail.com>
2019-06-28 22:14:34 +0100

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const utils = require("./src/utils/utils");
 const Flight = require("./src/models/flight");
 const Position = require("./src/models/position");
 const TimedPosition = require("./src/models/timedPosition");
-const mysql = require("./src/db/mysql");
+//const mysql = require("./src/db/mysql");
 const mongo = require("./src/db/mongoDB");
 
 exports.printMsg = () => {
--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -6,24 +6,46 @@ const DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 const MongoClient = require("mongodb").MongoClient;
 const client = new MongoClient(HOST);
 
+let db;
+
+module.exports = {
+  connect() {
+    return new Promise((resolve, reject) => {
+      client.connect(err => {
+        if (err) return reject(err);
+        console.log("Connected successfully to server");
+        db = client.db(DB_NAME);
+        resolve(db);
+      });
+    });
+  },
+  getDb() {
+    return db;
+  }
+};
+
+/*
 (async function() {
   try {
     await client.connect();
     console.log("Connected correctly to server");
     const db = client.db(DB_NAME);
-    const col = db.collection("test");
-    let r = await col.insertOne({ a: 1 });
+    const col = db.collection("flights");
 
-    const cursor = col.find({ a: 1 }).limit(2);
-    while (await cursor.hasNext()) {
-      const doc = await cursor.next();
-      console.dir(doc);
-    }
   } catch (err) {
     console.log(err.stack);
   }
   client.close();
 })();
+*/
+
+/*
+    const cursor = col.find({ a: 1 }).limit(2);
+    while (await cursor.hasNext()) {
+      const doc = await cursor.next();
+      console.dir(doc);
+    }
+    */
 
 const indexCollection = function(db, callback) {
   db.collection("documents").createIndex({ a: 1 }, null, function(
@@ -36,10 +58,15 @@ const indexCollection = function(db, callback) {
 };
 
 const writeToDbPassages = item => {
-
+  //let r = await col.insertOne({ a: 1 });
+  let modItem = {
+    ...item
+  };
+  modItem.startTime = modItem.startTime / 1000;
+  modItem.minDTimestamp = modItem.minDTimestamp / 1000;
 };
 
-exports.writeToDbPassagesMongo = writeToDbPassages;
+//exports.writeToDbPassagesMongo = writeToDbPassages;
 
 /*
 db.Collection.find({
