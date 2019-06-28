d0aea03656936a6b7a89688cfd3a849db2b5131b
Emanuele Fumagalli <emanuelef@gmail.com>
2019-06-28 20:20:54 +0100

    fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Flight = require("./src/models/flight");
 const Position = require("./src/models/position");
 const TimedPosition = require("./src/models/timedPosition");
 const mysql = require("./src/db/mysql");
+const mongo = require("./src/db/mongoDB");
 
 exports.printMsg = () => {
   console.log("Test message");
@@ -13,3 +14,4 @@ exports.Flight = Flight;
 exports.Position = Position;
 exports.TimedPosition = TimedPosition;
 exports.mysql = mysql;
+exports.mongo = mongo;
